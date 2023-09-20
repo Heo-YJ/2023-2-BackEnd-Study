b41fc1c87976ac70351c2787f556359e695112e7
허예진 <heoyejin9178@gmail.com>
2023-09-20 16:37:32 +0900

    Week1 과제 제출

---
+++ GDSC_1st_HW/src/main/java/org/example/Main.java
@@ -0,0 +1,41 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator() {
+            @Override
+            public int plus(int a, int b) {
+                return a+b;
+            }
+
+            @Override
+            public int minus(int a, int b) {
+                return a-b;
+            }
+
+            @Override
+            public int mul(int a, int b) {
+                return a*b;
+            }
+
+            @Override
+            public int div(int a, int b) {
+                return a/b;
+            }
+        };
+
+        System.out.println("1234 + 4321 = " + calculator.plus(1234, 4321));
+        System.out.println("911 - 170 = " + calculator.minus(911, 170));
+        System.out.println("2 * 3 = " + calculator.mul(2, 3));
+        System.out.println("33 / 11 = " + calculator.div(33, 11));
+
+        System.out.println("\n");
+
+        Cat cat = new Cat("cat"); // 다른 동물 하셔도 상관 없습니다!
+        Dog dog = new Dog("dog");
+        Animal[] animals = {cat, dog};
+        for (Animal animal : animals) {
+            animal.speak();
+        }
+    }
+}
